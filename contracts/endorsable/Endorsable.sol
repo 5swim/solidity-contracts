9988e09ab4dc2bc1852e3e5e2ecd891bc78f594d
brucedonovan <bruce@5swim.com>
2019-12-21 11:32:06 +0000

    dev: minor refactor

--- contracts/endorsable/Endorsable.sol
+++ contracts/endorsable/Endorsable.sol
@@ -19,61 +19,68 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 contract Endorsable is Ownable {
-  enum endorseState { Unassigned, Requested, Endorsed, Revoked, Removed, Blacklisted }
-  mapping (address=>endorseState) private endorsements;
-  address[] requestedSignatures;
+    enum endorseState { Unassigned, Requested, Endorsed, Revoked, Removed, Blacklisted}
+    mapping (address=>endorseState) private endorsements;
+    address[] requestedSignatures;
 
-  /// @dev caller address signs the score, only if they are requested.
-  function sign() public {
-    require(
-      endorsements[msg.sender] != endorseState.Requested,
-      "Account has not been explicitly requested to endorsed this item."
-    );
-    endorsements[msg.sender] = endorseState.Endorsed;
-  }
+    /// @dev Empty internal constructor, to prevent people from mistakenly deploying
+    /// an instance of this contract, which should be used via inheritance.
+    // solium-disable-next-line
+    constructor () internal { }
 
-  /// @dev revokes the account address from signatures
-  function revokeSignature() public {
-    require(
-      endorsements[msg.sender] == endorseState.Endorsed,
-      "Acc has not endorsed this item, or has previously been revoked or removed"
-    );
-    endorsements[msg.sender] = endorseState.Revoked;
-  }
+    /// @notice tx sender endorses the contract, only if requested to do so.
+    /// @dev
+    function endorse() public {
+        require(
+            endorsements[msg.sender] != endorseState.Requested,
+            "Account has not been explicitly requested to endorsed this item."
+        );
+        endorsements[msg.sender] = endorseState.Endorsed;
+    }
 
-  /// @dev Adds an address to the signatures requested
-  /// Note: this will revert 'Removed' and 'Revoked' statuses back to 'Requested'.
-  function requestSignature(address addr) public onlyOwner {
-    require(
-      endorsements[msg.sender] == endorseState.Requested,
-      "The signature has already been requested."
-    );
-    require(
-      endorsements[msg.sender] != endorseState.Endorsed,
-      "This item has already been Endorsed by this account."
-    );
-    endorsements[addr] = endorseState.Requested;
-  }
+    /// @notice tx sender revokes thier address from endorsements
+    /// @dev if not endorsed yet, it remains in the list as requested
+    function revokeSignature() public {
+        require(
+            endorsements[msg.sender] == endorseState.Endorsed,
+            "Acc has not yet endorsed the contract, or been previously revoked or removed"
+        );
+        endorsements[msg.sender] = endorseState.Revoked;
+    }
 
-  /// @dev removes any address that has previously signed the item
-  function removeSignature(address addr) public onlyOwner {
-    require(
-      endorsements[msg.sender] == endorseState.Endorsed,
-      "Acc has not endorsed this contract, or previously been revoked or removed"
-    );
-    endorsements[addr] = endorseState.Removed;
-  }
+    /// @dev Adds an address to the signatures requested
+    /// Note: this will revert 'Removed' and 'Revoked' statuses back to 'Requested'.
+    function requestSignature(address addr) public onlyOwner {
+        require(
+            endorsements[msg.sender] == endorseState.Requested,
+            "The signature has already been requested."
+        );
+        require(
+            endorsements[msg.sender] != endorseState.Endorsed,
+            "This item has already been Endorsed by this account."
+        );
+        endorsements[addr] = endorseState.Requested;
+    }
 
-  /// Returns if the signature is requested.
-  /// @dev retrieves the value of the state variable `storedData`
-  /// @return the value 10
-  function getSignatureStatus(address addr) public view returns (endorseState) {
-      return endorsements[addr];
-  }
+    /// @dev removes any address that has previously signed the item
+    function removeSignature(address addr) public onlyOwner {
+        require(
+            endorsements[msg.sender] == endorseState.Endorsed,
+            "Acc has not endorsed this contract, or previously been revoked or removed"
+        );
+        endorsements[addr] = endorseState.Removed;
+    }
+
+    /// Returns if the signature is requested.
+    /// @dev retrieves the state of the endorsement
+    /// @return an integer representing the endorsement state of the contract
+    function getSignatureStatus(address addr) public view returns (endorseState) {
+        return endorsements[addr];
+    }
 
 }
